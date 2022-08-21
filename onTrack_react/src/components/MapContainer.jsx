3b0bee5de516c1a5bd22e624772632ac7a165f72
NathanMarquis <nathanamrquis23@gmail.com>
2022-08-21 01:02:16 -0600

    map progress

--- onTrack_react/src/components/MapContainer.jsx
+++ onTrack_react/src/components/MapContainer.jsx
@@ -1,32 +1,44 @@
-import { Map, Marker, PolyLine, GoogleApiWrapper } from "google-maps-react";
-import React, {Component} from 'react';
+import React, { useState, useEffect } from 'react';
+import { GoogleMap, LoadScript, Marker, InfoWindow } from '@react-google-maps/api';
 
-export class MapContainer extends Component {
-  render() {
-    style = {
-      width: '100%',
-      height: '100%'
-    };
+const mapkey = 'AIzaSyCh2j8eK3Hl0IKuSS9DOmXUixpRTEc7gsk'
 
-    onMapClicked = () => {
-      //handle click events
-    };
+const MapContainer = () => {
 
+  const mapStyles = {
+    height: "50vh",
+    width: "100%",
+    display: "flex"
+  };
 
-    return (
-    <Map 
-      google={this.props.google}
-      style={this.style}
-      initialCenter={{
-        lat: 40.854885,
-        lng: -88.081807
-      }}
-      zoom={15}
-      onClick={this.onMapClicked}>
-    </Map>
-    )
+  const defaultCenter = {
+    lat: 32.4228638826,
+    lng: -99.8391523672
   }
+
+  const [currentPosition, setCurrentPosition] = useState(null);
+
+  const success = position => {
+    const currentPosition = {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude
+    }
+    setCurrentPosition(currentPosition);
+  };
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(success);
+  }, [])
+
+  return (
+    <LoadScript
+      googleMapsApiKey={mapkey}>
+      <GoogleMap
+        mapContainerStyle={mapStyles}
+        zoom={13}
+        center={currentPosition || defaultCenter}>
+      </GoogleMap>
+    </LoadScript>
+  )
 }
-export default GoogleApiWrapper({
-  apiKey: 'AIzaSyCh2j8eK3Hl0IKuSS9DOmXUixpRTEc7gsk'
-})(MapContainer);
+export default MapContainer;
